457015dbffb248429de994b5caf6fa6b732ee5bc
jetrotal <45118493+jetrotal@users.noreply.github.com>
2021-03-01 16:53:13 -0300

    Audio feedback is now OFF

--- jsEngines/audioEngine.js
+++ jsEngines/audioEngine.js
@@ -1,29 +1,29 @@
-function audioStart(){
+function audioStart() {
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    
+
     var aCtx;
     var analyser;
     var microphone;
     if (navigator.getUserMedia) {
-      navigator.getUserMedia({
-          audio: {
-            latency: 0.003,
-            echoCancellation: false,
-            mozNoiseSuppression: false,
-            mozAutoGainControl: false
-          }
-        },
-        function(stream) {
-          aCtx = new AudioContext();
-          microphone = aCtx.createMediaStreamSource(stream);
-          var destination = aCtx.destination;
-          microphone.connect(destination);
-        },
-        function() {
-          console.log("Error 003 - Chrome didn't allowed MIC Usage")
-        }
-      );
+        navigator.getUserMedia({
+                audio: {
+                    latency: 0.003,
+                    echoCancellation: false,
+                    mozNoiseSuppression: false,
+                    mozAutoGainControl: false
+                }
+            },
+            function(stream) {
+                aCtx = new AudioContext();
+                microphone = aCtx.createMediaStreamSource(stream);
+                var destination = aCtx.destination;
+                microphone.connect(destination);
+            },
+            function() {
+                console.log("Error 003 - Chrome didn't allowed MIC Usage")
+            }
+        );
     }
-    }
-    
-    audioStart();
+}
+
+//    audioStart();
